a6c69b8d8d8e84c728229eef6e0333bc9fe4180e
Diego Ferreira <katesclau@gmail.com>
2025-02-23 21:27:21 -0300

    Adds context helper to get values

--- pkg/helpers/context.go
+++ pkg/helpers/context.go
@@ -9,6 +9,7 @@ var ErrKeyNotFound = errors.New("Key not found in context")
 var ErrInvalidType = errors.New("Invalid type")
 
 // Get Typed Value from context
+// Ref: https://www.willem.dev/articles/how-to-add-values-to-context/
 func Get[T any](ctx context.Context, key string) (T, error) {
 	var ret T
 	v := ctx.Value(key)
